39becf72ee8dc1381ddf13ff81e08fe82d5462be
alireza vafa <alireza.vafa@gmail.com>
2026-07-26 12:34:36 +0330

    Add temperature converter

--- Parking Management/menu.py
+++
@@ -1,2 +0,0 @@
-def show_many_items():
-		print(" \nOption Menu:\n---------------------\n1) Enter to parking\n2) Exit form parking\n3) Find by plate\n4) Parking list\n0) Exit")
---
+++ temperature_converter.py
@@ -0,0 +1,5 @@
+temperature = float(input("Enter Celsius temperature: "))
+
+fahrenheit = temperature * 9 / 5 + 32
+
+print(fahrenheit)
