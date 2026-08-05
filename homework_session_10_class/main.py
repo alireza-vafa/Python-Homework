e46e35f30398f9d6ada35ffc3d3fa86d7cda0403
alireza vafa <alireza.vafa@gmail.com>
2026-08-05 14:16:07 +0330

    Add homework_session_10_class

--- homework_session_10_class/main.py
+++ homework_session_10_class/main.py
@@ -1,1 +1,79 @@
-print("test")
+from phone_class import Phone
+
+
+phone_list = []
+
+
+while True:
+
+    print()
+    print("1) Add contact")
+    print("2) Find by name")
+    print("3) Find by family")
+    print("4) Show phone list")
+    print("0) Exit")
+
+    option = int(input("\nChoose an option: "))
+
+    if option == 0:
+        break
+
+    elif option == 1:
+
+        name = input("Name: ")
+        family = input("Family: ")
+        number = int(input("Phone #: "))
+        information = input("Information: ")
+
+        phone = Phone(name, family, number, information)
+
+        duplicate = False
+
+        for person in phone_list:
+
+            if person.number == phone.number:
+                duplicate = True
+
+            elif (
+                person.name == phone.name
+                and person.family == phone.family
+                and person.information == phone.information
+            ):
+                duplicate = True
+
+        if duplicate:
+            print("Duplicate contact!")
+
+        else:
+            phone_list.append(phone)
+            print("Saved!")
+
+    elif option == 2:
+
+        name = input("Enter name: ").lower()
+
+        print("\nName      Family      Number      Information")
+        print("-----------------------------------------------")
+
+        for person in phone_list:
+            if person.name == name:
+                print(person)
+
+    elif option == 3:
+
+        family = input("Enter family: ").lower()
+
+        print("\nName      Family      Number      Information")
+        print("-----------------------------------------------")
+
+        for person in phone_list:
+            if person.family == family:
+                print(person)
+
+    elif option == 4:
+
+        print("\nName      Family      Number      Information")
+        print("-----------------------------------------------")
+
+        for person in phone_list:
+            print(person)
--- homework_session_10_class/phone_class.py
+++ homework_session_10_class/phone_class.py
@@ -1,2 +1,15 @@
-Class Phone:
-    pass
+class Phone:
+
+    def __init__(self, name, family, number, information):
+        self.name = name.lower()
+        self.family = family.lower()
+        self.number = number
+        self.information = information.lower()
+
+    def __str__(self):
+        return (
+            f"{self.name:10}"
+            f"{self.family:12}"
+            f"{self.number:<12}"
+            f"{self.information}"
+        )
